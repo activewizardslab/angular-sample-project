eb3a85558d949956e058d32e147ba3d1bfbdf38a
yaroslav-dudar <flayingfog@gmail.com>
2016-03-22 12:28:40 +0000

    / to login page

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', include('myapps.users.urls', namespace='users')),
     url(r'^dashboard/', include('myapps.dashboard.urls', namespace='dashboard')),
+    url(r'^$', RedirectView.as_view(url='/auth/login/'))
 ]
